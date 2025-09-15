2d7e81819fe6bf521d58e3d61eca00332c3f7e7d
Naveira <manuelnaveira7@gmail.com>
2025-09-15 22:20:01 +0200

    Proyecto inicial con firma GPG

---
+++ src/test/java/com/proyecto/clubs/carreras/CarrerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.clubs.carreras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
